e2ca8e9f5a930340ad5970660aff0e82257208f6
Pellikka Minna <h01340@haaga-helia.fi>
2023-10-25 12:11:19 +0300

    inserted system.out.println

--- src/main/java/s23/dogbackend/DogbackendApplication.java
+++ src/main/java/s23/dogbackend/DogbackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DogbackendApplication {
 
 	public static void main(String[] args) {
+		System.out.println("testien testi");
 		SpringApplication.run(DogbackendApplication.class, args);
 	}
 
